f50c495fdfa1efc97e144c667eca80318d0f5472
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2025-12-21 01:50:28 -0800

    chore: submit Ex.1 in 8.1 Objects and Methods

--- part08-01_smallest_average/src/smallest_average.py
+++ part08-01_smallest_average/src/smallest_average.py
@@ -1,1 +1,28 @@
 # Write your solution here
+
+def smallest_average(person1: dict, person2: dict, person3: dict) -> dict: 
+    person_list = [person1, person2, person3]
+    smallest_average = 0
+    track_person = 0
+    
+    # calculate average with loop
+    for index, person in enumerate(person_list):
+        sum = person["result1"] + person["result2"] + person["result3"]
+        avg = sum / 3
+
+        if index == 0:
+            smallest_average = avg
+            track_person = index
+        else:
+            if avg < smallest_average:
+                smallest_average = avg
+                track_person = index 
+    
+    return person_list[track_person]
+
+    
+# person1 = {"name": "Mary", "result1": 2, "result2": 3, "result3": 3}
+# person2 = {"name": "Gary", "result1": 5, "result2": 1, "result3": 8}
+# person3 = {"name": "Larry", "result1": 3, "result2": 1, "result3": 1}
+
+# print(smallest_average(person1, person2, person3))
