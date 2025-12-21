d98be5c03918b4c2ad2c0238cdc412a4d7d3b92f
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2025-12-21 02:07:24 -0800

    chore: submit Ex.2 in 8.1 Objects and Methods

--- part08-02_row_sums/src/row_sums.py
+++ part08-02_row_sums/src/row_sums.py
@@ -1,1 +1,15 @@
 # Write your solution here
+
+def row_sums(my_matrix: list) -> None:
+
+    for row in my_matrix:
+        row_sum = 0
+
+        for item in row:
+            row_sum += item
+
+        row.append(row_sum)
+
+# my_matrix = [[1, 2], [3, 4]]
+# row_sums(my_matrix)
+# print(my_matrix)
