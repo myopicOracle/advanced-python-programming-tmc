e49a1494bc9e7bdbad05a9efa8868aae789a9556
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2025-12-21 10:26:44 -0800

    chore: submit Ex.3-4 in 8.2 Classes and Objects

--- part08-04_shopping_list/src/shopping_list.py
+++ part08-04_shopping_list/src/shopping_list.py
@@ -19,3 +19,20 @@ class ShoppingList:
 # -------------------------
 # Write your solution here:
 # -------------------------
+
+def total_units(my_list: ShoppingList) -> int:
+    sum = 0
+
+    for i in range(1, my_list.number_of_items() + 1):
+        sum += my_list.amount(i)
+
+    return sum 
+
+
+if __name__ == "__main__":
+    my_list = ShoppingList()
+    my_list.add("bananas", 10)
+    my_list.add("apples", 5)
+    my_list.add("pineapple", 1)
+
+    print(total_units(my_list))
