fa3be97286bd6a1d671940662855cec5fbdbb5bc
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2025-12-30 13:20:41 -0800

    chore: submit Ex.1-2 in 9.1 Objs and Refs

--- part09-01_fastest_car/src/fastest_car.py
+++ part09-01_fastest_car/src/fastest_car.py
@@ -5,7 +5,29 @@ class Car:
         self.make = make
         self.top_speed = top_speed
 
-   def __str__(self):
+    def __str__(self):
         return f"Car (make: {self.make}, top speed: {self.top_speed})"
 
 # WRITE YOUR SOLUTION HERE:
+
+def fastest_car(cars_list: list) -> str:
+    top_speed = 0
+
+    for car in cars_list:
+        if car.top_speed > top_speed:
+            top_speed = car.top_speed
+            fastest_car = car.make
+
+    return fastest_car
+
+
+# TEST:
+
+if __name__ == "__main__":
+    car1 = Car("Saab", 195)
+    car2 = Car("Lada", 110)
+    car3 = Car("Ferrari", 280)
+    car4 = Car("Trabant", 85)
+
+    cars = [car1, car2, car3, car4]
+    print(fastest_car(cars))
