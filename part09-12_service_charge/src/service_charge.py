597ebf5ef6a424256aacec691974dc8ae4ca311a
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-01-01 03:14:32 -0800

    chore: submit Ex.12 in 9.4 Scope of Methods

--- part09-12_service_charge/src/service_charge.py
+++ part09-12_service_charge/src/service_charge.py
@@ -1,3 +1,38 @@
 # WRITE YOUR SOLUTION HERE:
 
+class BankAccount: 
+    def __init__(self, owner: str, account: int, balance: float):
+        self.__owner = owner
+        self.__account = account
+        self.__balance = balance
 
+    @property
+    def balance(self):
+        return self.__balance
+
+    def deposit(self, amount: float):
+        if amount > 0:
+            self.__balance += amount
+            self.__service_charge()
+        else:
+            raise ValueError("Amount must be positive.")
+
+    def withdraw(self, amount: float):
+        if amount <= self.__balance:
+            self.__balance -= amount
+            self.__service_charge()
+        else: 
+            raise ValueError("Insufficient funds for withdrawal.")
+
+    def __service_charge(self):
+        if self.__balance > 0:
+            self.__balance *= 0.99
+        else:
+            raise ValueError("Insufficient funds for service charge.")
+
+
+# account = BankAccount("Randy Riches", "12345-6789", 1000)
+# account.withdraw(100)
+# print(account.balance)
+# account.deposit(100)
+# print(account.balance)
