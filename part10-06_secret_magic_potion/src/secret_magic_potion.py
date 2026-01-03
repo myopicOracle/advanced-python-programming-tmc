b03172c52d8cef7e1324386ccdea46bedaba4041
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-01-02 18:42:43 -0800

    chore: submit Ex.5-6 in 10.2 Access Modifiers

--- part10-06_secret_magic_potion/src/secret_magic_potion.py
+++ part10-06_secret_magic_potion/src/secret_magic_potion.py
@@ -13,3 +13,28 @@ class MagicPotion:
             print(f"{ingredient[0]} {ingredient[1]} grams")
 
 
+class SecretMagicPotion(MagicPotion):
+    def __init__(self, name: str, password: str):
+        super().__init__(name)
+        self.__password = password
+
+    def add_ingredient(self, ingredient: str, amount: float, password: str):
+        if password == self.__password:
+            super().add_ingredient(ingredient, amount)
+        else: 
+            raise ValueError("Wrong password!")
+
+    def print_recipe(self, password: str):
+        if password == self.__password:
+            super().print_recipe()
+        else: 
+            raise ValueError("Wrong password!")
+
+
+# diminuendo = SecretMagicPotion("Diminuendo maximus", "hocuspocus")
+# diminuendo.add_ingredient("Toadstool", 1.5, "hocuspocus")
+# diminuendo.add_ingredient("Magic sand", 3.0, "hocuspocus")
+# diminuendo.add_ingredient("Frogspawn", 4.0, "hocuspocus")
+# diminuendo.print_recipe("hocuspocus")
+
+# diminuendo.print_recipe("pocushocus")
