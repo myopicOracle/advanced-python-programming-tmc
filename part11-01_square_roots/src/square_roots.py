f41fa5c966d473166be6caffcc81ff29718507bd
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-01-04 22:57:00 -0800

    chore: submit Ex.1-4 in 11.1 List Comprehension

---
+++ part11-01_square_roots/src/square_roots.py
@@ -0,0 +1,11 @@
+# WRITE YOUR SOLUTION HERE:
+
+import math
+
+def square_roots(numbers: list) -> list:
+    return [math.sqrt(num) for num in numbers]
+
+
+# lines = square_roots([1,2,3,4])
+# for line in lines:
+#     print(line)
---
+++ part11-04_lengths/src/lengths.py
@@ -0,0 +1,8 @@
+# WRITE YOUR SOLUTION HERE:
+
+def lengths(lists: list) -> list:
+    return [len(item) for item in lists]
+
+
+# lists = [[1,2,3,4,5], [324, -1, 31, 7],[]]
+# print(lengths(lists))
