a53f752f58456231e63877be4eb56f1b5a7d0d24
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-01-04 17:53:49 -0800

    chore: submit Ex.11-12 in 10.4 Larger Apps

--- part10-11_phone_book_v2/src/phone_book_v2.py
+++ part10-11_phone_book_v2/src/phone_book_v2.py
@@ -5,17 +5,45 @@ class PhoneBook:
         self.__persons = {}
 
     def add_number(self, name: str, number: str):
-        if not name in self.__persons:
-            self.__persons[name] = []
-        self.__persons[name].append(number)
+        if name not in self.__persons:
+            self.__persons[name] = Person(name)
+        self.__persons[name].add_number(number)
+
+    def add_address(self, name: str, address: str):
+        if name not in self.__persons:
+            self.__persons[name] = Person(name)
+        self.__persons[name].add_address(address)
 
     def get_entry(self, name: str):
-        if not name in self.__persons:
+        if name not in self.__persons:
             return None
         return self.__persons[name]
 
     def all_entries(self):
         return self.__persons
+
+
+class Person: 
+    def __init__(self, name: str):
+        self.__name = name
+        self._numbers = []
+        self._address = None 
+
+    def name(self):
+        return self.__name
+
+    def numbers(self):
+        return self._numbers
+
+    def address(self):
+        return self._address
+
+    def add_number(self, number: str):
+        self._numbers.append(number)
+
+    def add_address(self, address: str):
+        self._address = address
+
 
 class PhoneBookApplication:
     def __init__(self):
@@ -26,20 +54,37 @@ class PhoneBookApplication:
         print("0 exit")
         print("1 add number")
         print("2 search")
+        print("3 add address")
 
     def add_number(self):
         name = input("name: ")
         number = input("number: ")
         self.__phonebook.add_number(name, number)
 
+    def add_address(self):
+        name = input("name: ")
+        address = input("address: ")
+        self.__phonebook.add_address(name, address)
+
     def search(self):
         name = input("name: ")
-        numbers = self.__phonebook.get_entry(name)
-        if numbers == None:
-            print("number unknown") 
-            return 
-        for number in numbers:
-            print(number)       
+        person = self.__phonebook.get_entry(name)
+        if person is None:
+            print("number unknown")
+            print("address unknown")
+        else:
+            numbers = person.numbers()
+            if numbers:
+                for number in numbers:
+                    print(number)
+            else:
+                print("number unknown")
+            
+            address = person.address()
+            if address:
+                print(address)
+            else:
+                print("address unknown")  
 
     def execute(self):
         self.help()
@@ -52,6 +97,8 @@ class PhoneBookApplication:
                 self.add_number()
             elif command == "2":
                 self.search()
+            elif command == "3":
+                self.add_address()
             else:
                 self.help()
 
--- part10-12_course_records/src/course_records.py
+++ part10-12_course_records/src/course_records.py
@@ -1,1 +1,129 @@
 # tee ratkaisusi tänne
+
+class Transcript:
+    def __init__(self):
+        self.__transcript = {}
+
+    def __str__(self):
+        num_of_courses, total_credits, mean, grades = self.__calculate_stats()
+
+        string = ""
+        string += f"{num_of_courses} completed courses, a total of {total_credits} credits\n"
+        string += f"mean {mean:.1f}\n"
+        string += f"grade distribution\n"
+        string += f"5: {grades.count(5) * 'x'}\n"
+        string += f"4: {grades.count(4) * 'x'}\n"
+        string += f"3: {grades.count(3) * 'x'}\n"
+        string += f"2: {grades.count(2) * 'x'}\n"
+        string += f"1: {grades.count(1) * 'x'}"
+
+        return string
+
+    @property
+    def transcript(self):
+        return self.__transcript
+
+    def add_course(self, name: str, grade: str, credits: str):
+        course = self.__transcript.get(name)
+        if course: 
+            course.grade = grade
+        else: 
+            course = Course(name, grade, credits)
+            self.__transcript[name] = course
+
+    def get_course(self, name: str):
+        course = self.__transcript.get(name)
+        if course: 
+            return course
+        else:
+            return "no entry for this course"
+
+    def __calculate_stats(self):
+        grades = []
+        credits = []
+
+        for course in self.transcript.values():
+            grades.append(int(course.grade))
+            credits.append(int(course.credits))
+
+        num_of_courses = len(self.transcript)
+        total_credits = sum(credits)
+        mean = sum(grades) / num_of_courses
+
+        return num_of_courses, total_credits, mean, grades
+
+
+class Course:
+    def __init__(self, name: str, grade: str, credits: str):
+        self.__name = name
+        self.__grade = grade
+        self.__credits = credits
+
+    def __str__(self):
+        return f"{self.name} ({self.credits} cr) grade {self.grade}"
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def grade(self):
+        return self.__grade
+
+    @property
+    def credits(self):
+        return self.__credits
+
+    @grade.setter
+    def grade(self, new_grade: str):
+        if int(new_grade) > int(self.grade):
+            self.__grade = new_grade
+
+
+class ApplicationInterface:
+    def __init__(self):
+        self.transcript = Transcript()
+
+    def help(self):
+        print("1 add course")
+        print("2 get course data")
+        print("3 statistics")
+        print("0 exit")
+
+    def add_course(self):
+        course = input("course: ")
+        grade = input("grade: ")
+        credits = input("credits: ")
+        self.transcript.add_course(course, grade, credits)
+
+    def get_course(self):
+        course = input("course: ")
+        print(self.transcript.get_course(course))
+
+    def print_stats(self):
+        print(self.transcript)
+
+    def execute(self):
+        self.help()
+        print()
+        while True:
+            command = int(input("command: "))
+            if command == 0:
+                break
+            elif command == 1: 
+                self.add_course()
+                print()
+            elif command == 2: 
+                self.get_course()
+                print()
+            elif command == 3: 
+                self.print_stats()
+                print()
+            else:
+                print("invalid command")
+                self.help()
+                print()
+
+
+app = ApplicationInterface()
+app.execute()
