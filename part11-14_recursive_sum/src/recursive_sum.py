899fce91c740c5c52c82af2ed1303f55ffe93678
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-01-06 14:01:29 -0800

    chore: submit Ex.13-15 in 11.3 Recursion

--- part11-14_recursive_sum/src/recursive_sum.py
+++ part11-14_recursive_sum/src/recursive_sum.py
@@ -1,1 +1,16 @@
 # WRITE YOUR SOLUTION HERE:
+
+def recursive_sum(number: int):
+    # if the number is 1, there is nothing else to add
+    if number <= 1:
+        return number
+
+    # fill in the rest of the function
+    return recursive_sum(number-1) + number
+
+
+# result = recursive_sum(3)
+# print(result)
+
+# print(recursive_sum(5))
+# print(recursive_sum(10))
--- part11-15_balanced_brackets/src/balanced_brackets.py
+++ part11-15_balanced_brackets/src/balanced_brackets.py
@@ -1,9 +1,25 @@
 
-def balanced_brackets(my_string: str):
+def balanced_brackets(string: str):
+    my_string = ''.join([char for char in string if char in '()[]'])
+    # print('cleaned string:', my_string) # debug
+
     if len(my_string) == 0:
         return True
-    if not (my_string[0] == '(' and my_string[-1] == ')'):
+    if not ((my_string[0] == '(' and my_string[-1] == ')') or (my_string[0] == '[' and my_string[-1] == ']')):
         return False
 
     # remove first and last character
     return balanced_brackets(my_string[1:-1])
+
+
+# ok = balanced_brackets("([([])])")
+# print(ok)
+
+# ok = balanced_brackets("(python version [3.7]) please use this one!")
+# print(ok)
+
+# ok = balanced_brackets("(()]")
+# print(ok)
+
+# ok = balanced_brackets("([bad egg)]")
+# print(ok)
