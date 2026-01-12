68b914d8f2ff7f785e81a4862b393abeff92c40a
Gary Xia (myopicOracle) <184602237+myopicOracle@users.noreply.github.com>
2026-01-12 14:35:26 +0700

    chore: submit Ex.14-15 in 12.4 RegEx

--- part12-14_regular_expressions/src/regular_expressions.py
+++ part12-14_regular_expressions/src/regular_expressions.py
@@ -1,2 +1,36 @@
 # Write your solution here
 import re
+
+def regex(pattern: str, string: str):
+    return bool(re.search(pattern, string))
+
+def is_dotw(my_string: str) -> bool: 
+    pattern = "Mon.*|Tue.*|Wed.*|Thu.*|Fri.*|Sat.*|Sun.*"
+    return regex(pattern, my_string)
+
+def all_vowels(my_string: str) -> bool:
+    pattern = "^[aeiou]+$"
+    return regex(pattern, my_string)
+
+def time_of_day(my_string: str) -> bool: 
+    pattern = "^([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$"
+    return regex(pattern, my_string)
+
+
+if __name__ == "__main__":
+
+    # Part 1
+    print(is_dotw("Mon"))
+    print(is_dotw("Fri"))
+    print(is_dotw("Tui"))
+
+    # Part 2
+    print(all_vowels("eioueioieoieou"))
+    print(all_vowels("autoooo"))
+
+    # Part 3
+    print(time_of_day("12:43:01"))
+    print(time_of_day("AB:01:CD"))
+    print(time_of_day("17:59:59"))
+    print(time_of_day("33:66:77"))
+    
--- part12-15_hockey_statistics/src/hockey_statistics.py
+++ part12-15_hockey_statistics/src/hockey_statistics.py
@@ -1,1 +1,122 @@
 # Write your solution here
+
+import json
+
+
+def read_file():
+    file_name = input("file name: ")
+
+    with open(file_name) as f:
+        raw = f.read()
+
+    data = json.loads(raw)
+    print(f"read the data of {len(data)} players")
+
+    return data
+
+
+def show_help():
+    print("commands:")
+    print("0 quit")
+    print("1 search for player")
+    print("2 teams")
+    print("3 countries")
+    print("4 players in team")
+    print("5 players from country")
+    print("6 most points")
+    print("7 most goals")
+
+
+def player_string(obj: dict):
+    name = f"{obj['name']:<21}"
+    team = f"{obj['team']:>3}"
+    goals = f"{obj['goals']:>2}"
+    assists = f"{obj['assists']:>2}"
+    total = f"{obj['goals'] + obj['assists']:>3}"
+    
+    return f"{name}{team}  {goals} + {assists} = {total}"
+
+
+def search_for_player(player_name: str, data: list):
+    obj = [player for player in data if player["name"] == player_name][0]
+    print(player_string(obj))
+
+
+def get_teams(data: list):
+    return sorted(list(set([player['team'] for player in data])))
+
+def get_countries(data: list):
+    return sorted(list(set([player['nationality'] for player in data])))
+
+
+def filter_by_teams(data: list, team: str):
+    return sorted([player for player in data if player['team'] == team], key = lambda x : x['goals'] + x['assists'], reverse = True)
+
+def filter_by_country(data: list, country: str):
+    return sorted([player for player in data if player['nationality'] == country], key = lambda x : x['goals'] + x['assists'], reverse = True)
+
+
+def most_points(data: list, limit: int): 
+    sorted_list = sorted(data, key = lambda x : (x['goals'] + x['assists'], x['goals']), reverse = True)
+    return sorted_list[:limit]
+
+def most_goals(data: list, limit: int): 
+    sorted_list = sorted(data, key = lambda x : (x['goals'], -x['games']), reverse = True)
+    return sorted_list[:limit]
+
+
+def execute():
+    data = read_file()
+    show_help()
+
+    while True: 
+        command = int(input("command: "))
+
+        if command == 0:
+            break
+
+        elif command == 1: 
+            player_name = input("name: ")
+            search_for_player(player_name, data)
+
+        elif command == 2: 
+            teams = get_teams(data)
+            for team in teams:
+                print(team)
+
+        elif command == 3:
+            countries = get_countries(data)
+            for country in countries:
+                print(country)
+
+        elif command == 4:
+            team = input("team: ")
+            players_list = filter_by_teams(data, team)
+            for player in players_list:
+                print(player_string(player))
+
+        elif command == 5:
+            country = input("country: ")
+            players_list = filter_by_country(data, country)
+            for player in players_list:
+                print(player_string(player))
+
+        elif command == 6:
+            limit = int(input("how many: "))
+            players_list = most_points(data, limit)
+            for player in players_list:
+                print(player_string(player))
+
+        elif command == 7:
+            limit = int(input("how many: "))
+            players_list = most_goals(data, limit)
+            for player in players_list:
+                print(player_string(player))
+
+
+execute()
+
+
+
+
+
